c7a7f1c3731dc4cb5991bad05928fabb7faac3fc
DevWurm <devwurm@gmx.net>
2015-11-12 17:01:47 +0100

    Adapting pageInterpreterTests

--- test/interpreters/pageInterpreterTests.js
+++ test/interpreters/pageInterpreterTests.js
@@ -1,12 +1,12 @@
 var pageInterpreter = require("../../lib/interpreters/pageInterpreter");
 var assert = require('assert');
 
-suite("pageInterpreter setup and interpretation", function() {
+suite("pageInterpreter", function() {
     suite("interpretation", function() {
         test("file interpretation", function (done) {
             var filePath = "test/resources/testFile.njssp";
 
-            var testedPageInterpreter = new pageInterpreter.pageInterpreter();
+            var testedPageInterpreter = new pageInterpreter();
 
             testedPageInterpreter.interpretFile(filePath, function (result) {
                 assert.equal(result, "<html><head></head><body>Testtext</body></html>");
